1b86bc00c720e084473906864e2082370e15a275
YOrtiz <yortiz@blumonpay.com.mx>
2023-08-03 00:34:24 -0600

    Se añade primera vista y select de tipo de moneda

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+
+    &:hover {
+        background-color: #7A7DFE;
+        cursor: pointer;
+    }
+`
+
+function Formulario() {
+
+    const [state, SelectMonedas] = useSelectMonedas('Elige tu Moneda', monedas);
+
+    return (
+
+        <form>
+
+            <SelectMonedas />
+
+            {state}
+
+            <InputSubmit
+                type="submit"
+                value="Cotizar"
+            />
+        </form>
+    )
+}
+
+export default Formulario
