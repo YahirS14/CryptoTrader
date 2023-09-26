bfacf8023b5c8e65496670b5f0aa743d11464e52
YOrtiz <yortiz@blumonpay.com.mx>
2023-09-26 11:59:55 -0600

    Version 3.0.0 - °Se agrega spinner de carga al cotizar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import Formulario from './components/Formulario';
 import Resultado from './components/Resultado';
+import Spinner from './components/Spinner';
 import ImagenCrypto from './img/imagen-criptos.png';
 
 //Styles Components
@@ -43,19 +44,22 @@ const Heading = styled.h1`
 function App() {
   const [monedas, setMonedas] = useState({});
   const [resultado, setResultado] = useState({});
+  const [cargando, setCargando] = useState(false);
 
   useEffect(() => {
     if (Object.keys(monedas).length > 0) {
       const cotizaCripto = async () => {
+        setCargando(true);
+        setResultado({});
         const { moneda, criptoMoneda } = monedas;
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptoMoneda}&tsyms=${moneda}`;
 
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
 
-        setResultado(
-          resultado.DISPLAY[criptoMoneda][moneda]
-        ); //-> Busca las propiedades del objeto
+        setResultado(resultado.DISPLAY[criptoMoneda][moneda]); //-> Busca las propiedades del objeto
+
+        setCargando(false);
       };
 
       cotizaCripto();
@@ -64,18 +68,14 @@ function App() {
 
   return (
     <Contenedor>
-      <Imagen
-        src={ImagenCrypto}
-        alt='Imagenes Criptomonedas'
-      />
+      <Imagen src={ImagenCrypto} alt='Imagenes Criptomonedas' />
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
 
         <Formulario setMonedas={setMonedas} />
 
-        {resultado.PRICE && (
-          <Resultado resultado={resultado} />
-        )}
+        {cargando && <Spinner />}
+        {resultado.PRICE && <Resultado resultado={resultado} />}
       </div>
     </Contenedor>
   );
